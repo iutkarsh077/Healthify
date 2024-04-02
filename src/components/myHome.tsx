8d86852bf1647651a7ace54aa90e2a4f20cf79ac
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-04-02 07:21:07 +0530

    time zone

--- src/components/myHome.tsx
+++ src/components/myHome.tsx
@@ -3,8 +3,8 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 const MyHome = () => {
   const now = new Date();
 
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', timeZone: 'Asia/Kolkata' });
+  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeZone: 'Asia/Kolkata' })).format(now);
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
